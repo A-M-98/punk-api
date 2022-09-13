9bd6c87fbbda60ae2c881b4620d619e572b9751d
Afaq Munawar <afaqmunawar12@gmail.com>
2022-09-13 16:38:10 +0100

    got the props in to the Cards.

--- src/containers/CardList/CardList.jsx
+++ src/containers/CardList/CardList.jsx
@@ -4,20 +4,15 @@ import Card from '../../components/Card/Card'
 const CardList = (props) => {
     const { beerArr } = props;
     // const exampleBeer = beerArr[0]
-    // const {
-    //   name,
-    //   description,
-    //   image_url
-    // } = exampleBeer
 
   return (
     <div className='card-list'>
-      {beerArr.map((beers) => {
+      {beerArr.map((beer) => {
         return (
           <Card 
-          image={beerArr.image_url} 
-          name={beerArr.name} 
-          description={beerArr.description}/>
+          image={beer.image_url} 
+          name={beer.name} 
+          description={beer.description}/>
         )
       })
       }
